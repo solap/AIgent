7102d6ea32104d642fc28f58f22a8a634e8f4c1c
Joel Dehlin <solap@hotmail.com>
2026-01-31 19:58:03 -0700

    Initial Commit

---
+++ AIgent/AIgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIgentApp.swift
+//  AIgent
+//
+//  Created by Joel Dehlin on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct AIgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
